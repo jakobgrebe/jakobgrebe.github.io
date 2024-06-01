5a972aeb03f63ba003dea0248e30bb524ca54467
Jakob Grebe <114878202+jakobgrebe@users.noreply.github.com>
2024-05-31 22:30:45 -0600

    changes

--- commands.js
+++ commands.js
@@ -87,4 +87,5 @@ banner = [
   "<br>",
   '<span class="color2">Welcome to my interactive Terminal Portfolio.</span>',
   '<span class="color2">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+  "<br>",
 ];
